e2bfe6f14556f2eead5b67747a3d62ec059e6d98
toghrul <tjaffarov@gmail.com>
2023-12-05 13:46:27 -0800

    testt-toghrul

--- toghrul.sh
+++ toghrul.sh
@@ -7,7 +7,8 @@ run_tests() {
        egrep -q 'Item [12]' should-matches.txt && \
        egrep -q -v 'Item [35]' should-misses.txt && \
        egrep -q -v 'Item [46]' not-matches.txt && \
-       egrep -q 'Item [56]' not-misses.txt; then
+       egrep -q 'Item [56]' not-misses.txt; 
+    then
         echo "cmd success"
         exit 0
     else
